ac6cb84cb353fa52c759dae5054a201c3cc9c16f
Masayuki Onishi <omccello@gmail.com>
2020-05-04 14:30:59 +0900

    最低限のimageに修正

--- docker/entrypoint_for_assign_project.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -xe
-
-docker-compose run --rm app bundle install
-docker-compose run --rm app ./bin/rails db:create
-docker-compose run --rm app ./bin/rails db:migrate
-docker-compose run --rm app ./bin/rails db:seed_fu
-
-exec "$@"
--- docker/entrypoint_for_repo_clone.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -xe
-
-rm -rf .git
-git init
-git add .
-git commit -m "first commit"
-
-exec "$@"
